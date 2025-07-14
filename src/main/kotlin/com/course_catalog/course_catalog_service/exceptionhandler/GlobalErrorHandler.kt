16b562046372ed0f169a6e905ba49715afa696bc
Nataniel Richardt <nataniel.r.v@gmail.com>
2025-07-14 16:31:29 -0300

    feat: added custom error handler

---
+++ src/main/kotlin/com/course_catalog/course_catalog_service/exceptionhandler/GlobalErrorHandler.kt
@@ -0,0 +1,39 @@
+package com.course_catalog.course_catalog_service.exceptionhandler
+
+import mu.KLogging
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@Component
+@ControllerAdvice
+class GlobalErrorHandler : ResponseEntityExceptionHandler() {
+    companion object : KLogging() {
+        // Função de extensão para evitar conflito de acesso
+        fun logError(ex: MethodArgumentNotValidException) {
+            logger.error(ex) { "Validation error occurred: ${ex.message}" }
+        }
+    }
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<in Any>? {
+        logError(ex)
+
+        val errors = ex.bindingResult.allErrors
+            .map { error -> error.defaultMessage!! }
+            .sorted()
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(errors)
+    }
+
+}
