e125a9e0380c8a9a83c6eea920d25302eb3eef62
Nataniel Richardt <nataniel.r.v@gmail.com>
2025-07-14 15:21:13 -0300

    feat: added unit test to add course

---
+++ src/test/unit/CourseControllerUnitTest.kt
@@ -0,0 +1,38 @@
+import com.course_catalog.course_catalog_service.CourseCatalogServiceApplication
+import com.course_catalog.course_catalog_service.controller.CourseController
+import com.course_catalog.course_catalog_service.dto.CourseDTO
+import com.course_catalog.course_catalog_service.service.CourseService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import jdk.jfr.Category
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@WebMvcTest(controllers = [CourseController::class])
+@ContextConfiguration(classes = [CourseCatalogServiceApplication::class])
+@AutoConfigureWebClient
+class CourseControllerUnitTest {
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @MockkBean
+    lateinit var courseServiceMock: CourseService
+
+    @Test
+    fun addCourse() {
+        val courseDTO = CourseDTO(null, "Test", "Cat Test")
+
+        every { courseServiceMock.addCourse(any()) } returns courseDTO(id = 1)
+    }
+
+    fun courseDTO(
+        id: Int? = null,
+        name: String = "Test",
+        category: String = "Cat Test"
+    ) = CourseDTO(id, name, category)
+}
