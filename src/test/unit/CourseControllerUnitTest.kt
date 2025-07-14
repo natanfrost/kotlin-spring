08ed1a52734d4ffdfb6933db0369c885142edcb4
Nataniel Richardt <nataniel.vieira@terceiro.rnp.br>
2025-07-14 15:24:58 -0300

    test: added unit test to get all courses

--- src/test/unit/CourseControllerUnitTest.kt
+++ src/test/unit/CourseControllerUnitTest.kt
@@ -5,6 +5,7 @@ import com.course_catalog.course_catalog_service.service.CourseService
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import jdk.jfr.Category
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
@@ -28,6 +29,24 @@ class CourseControllerUnitTest {
         val courseDTO = CourseDTO(null, "Test", "Cat Test")
 
         every { courseServiceMock.addCourse(any()) } returns courseDTO(id = 1)
+    }
+
+    @Test
+    fun getAllCourses() {
+        every { courseServiceMock.getAll() }.returnsMany(
+            listOf(courseDTO(id = 1),
+                courseDTO(id = 2, name = "Test"))
+        )
+        val savedCourseDTO = webTestClient
+            .get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertEquals(2, savedCourseDTO!!.size)
     }
 
     fun courseDTO(
