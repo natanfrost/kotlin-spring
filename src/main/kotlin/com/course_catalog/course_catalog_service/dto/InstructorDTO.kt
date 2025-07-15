912d6f563545b6d4280b199828d397f2bd3258c8
Nataniel Richardt <nataniel.vieira@terceiro.rnp.br>
2025-07-15 11:31:26 -0300

    fix: adjusted annotations from dto to entity

--- src/main/kotlin/com/course_catalog/course_catalog_service/dto/InstructorDTO.kt
+++ src/main/kotlin/com/course_catalog/course_catalog_service/dto/InstructorDTO.kt
@@ -1,18 +1,9 @@
 package com.course_catalog.course_catalog_service.dto
 
-import com.course_catalog.course_catalog_service.entity.Course
-import jakarta.persistence.CascadeType
-import jakarta.persistence.OneToMany
 import jakarta.validation.constraints.NotBlank
 
 data class InstructorDTO(
     val id: Int?,
     @get:NotBlank(message = "instructorDTO.name must not be blank")
-    var name: String,
-    @OneToMany(
-        mappedBy = "instructor",
-        cascade = [CascadeType.ALL],
-        orphanRemoval = true
-    )
-    var courses : List<Course> = mutableListOf()
+    var name: String
 )
--- src/main/kotlin/com/course_catalog/course_catalog_service/entity/Instructor.kt
+++ src/main/kotlin/com/course_catalog/course_catalog_service/entity/Instructor.kt
@@ -1,16 +1,24 @@
 package com.course_catalog.course_catalog_service.entity
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "Instructor")
+@Table(name = "Instructors")
 data class Instructor(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Int?,
-    val name: String
+    val name: String,
+    @OneToMany(
+        mappedBy = "instructor",
+        cascade = [CascadeType.ALL],
+        orphanRemoval = true
+    )
+    val courses : List<Course> = mutableListOf()
 )
