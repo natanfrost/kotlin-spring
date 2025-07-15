c03e4544ce453eeed984eb554be3c442f69daf10
Nataniel Richardt <nataniel.vieira@terceiro.rnp.br>
2025-07-15 10:53:17 -0300

    feat: added get course by name endpoint

--- src/main/kotlin/com/course_catalog/course_catalog_service/repository/CourseRepository.kt
+++ src/main/kotlin/com/course_catalog/course_catalog_service/repository/CourseRepository.kt
@@ -4,5 +4,5 @@ import com.course_catalog.course_catalog_service.entity.Course
 import org.springframework.data.repository.CrudRepository
 
 interface CourseRepository : CrudRepository<Course, Int> {
-
+    fun findByNameContaining(courseName : String) : List<Course>
 }
