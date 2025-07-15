f76e5ce88932a774fe2084a9955eaecc122c9b52
Nataniel Richardt <nataniel.r.v@gmail.com>
2025-07-15 11:31:26 -0300

    fix: adjusted annotations from dto to entity

--- src/main/kotlin/com/course_catalog/course_catalog_service/dto/CourseDTO.kt
+++ src/main/kotlin/com/course_catalog/course_catalog_service/dto/CourseDTO.kt
@@ -1,9 +1,5 @@
 package com.course_catalog.course_catalog_service.dto;
 
-import com.course_catalog.course_catalog_service.entity.Instructor
-import jakarta.persistence.FetchType
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
 import jakarta.validation.constraints.NotBlank
 
 data class CourseDTO(
@@ -11,9 +7,6 @@ data class CourseDTO(
     @get:NotBlank(message = "courseDTO.name must not be blank")
     val name: String,
     @get:NotBlank(message = "courseDTO.category must not be blank")
-    val category: String,
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="INSTRUCTOR_ID", nullable = false)
-    val instructor: Instructor? =null
+    val category: String
 );
 
